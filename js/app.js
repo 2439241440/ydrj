4459b199e8520bd8e1a6ddc8496986a7ff9a853f
jian <2439241440@qq.com>
2017-04-17 16:05:02 +0800

    123

--- js/app.js
+++ js/app.js
@@ -6,9 +6,18 @@
 'use strict'
 
 angular.module('myApp',['ui.router','home','ksSwiper','homePage','serviceProcess','mass','cases','joinUs','aboutUs'])
+    .run(function($rootScope){
+        $rootScope.$on('$stateChangeStart', function(){
+            $rootScope.isLoading = true;
+        })
+        $rootScope.$on('$stateChangeStart', function(){
+            $rootScope.isLoading = false;
+        })
+    })
     .controller('mainController',function($rootScope){
         $rootScope.title='益等软件-首页';
         $rootScope.rootPath = '/ydrj';
+
     })
     .directive('width',function(){
         return {
