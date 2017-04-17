8b18972afac98cf12e4b38cb47d5cfab19bda83b
jian <2439241440@qq.com>
2017-04-17 16:27:25 +0800

    阿萨德发

--- js/app.js
+++ js/app.js
@@ -11,17 +11,28 @@ angular.module('myApp',['ui.router','home','ksSwiper','homePage','serviceProcess
             $rootScope.isLoading = true;
             console.log('加载中，，，');
         })
-        $rootScope.$on('$stateChangeSuccess', function(){
+      /*  $rootScope.$on('$stateChangeSuccess', function(){
+
+        })*/
+        $rootScope.$on('$viewContentLoaded', function(event, viewConfig){
+            $rootScope.isLoading = false;
+            console.log('加载完成');
+            console.log(event);
+        });
+    })
+    .controller('mainController',function($rootScope,$scope){
+        $rootScope.title='益等软件-首页';
+        $rootScope.rootPath = '/ydrj';
+        /*$scope.$on('$viewContentLoading', function(event, viewConfig){
+            $rootScope.isLoading = true;
+            console.log('加载中，，，');
+        });
+        $scope.$on('$viewContentLoaded', function(event, viewConfig){
             $timeout(function(){
                 $rootScope.isLoading = false;
                 console.log('加载完成');
             },100)
-        })
-    })
-    .controller('mainController',function($rootScope){
-        $rootScope.title='益等软件-首页';
-        $rootScope.rootPath = '/ydrj';
-
+        });*/
     })
     .directive('width',function(){
         return {
--- js/homePage.Controller.js
+++ js/homePage.Controller.js
@@ -5,4 +5,15 @@
 angular.module('homePage',[])
     .controller('homePage',function($scope,$rootScope){
         $rootScope.bar_index = 0;
+        /*$scope.$on('$viewContentLoading', function(event, viewConfig){
+            $rootScope.isLoading = true;
+            console.log('加载中，，，');
+            console.log(event);
+        });
+        $scope.$on('$viewContentLoaded', function(event, viewConfig){
+                $rootScope.isLoading = false;
+                console.log('加载完成');
+                console.log(event);
+        });*/
+
     })
